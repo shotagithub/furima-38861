5124bc6309c0acdd4e94fb643bc069117ba06700
shotagithub <shota19891031@icloud.com>
2023-01-29 17:04:49 +0900

    item destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,15 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :item_find, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :item_find, only: [:show, :edit, :update, :destroy]
 
   def index
     @item = Item.all.order(created_at: :desc)
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,12 +18,16 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  def show   
+
+  def show
   end
+
   def edit
     return if user_signed_in? && current_user.id == @item.user.id
+
     redirect_to root_path
   end
+
   def update
     @item.update(item_params)
     if @item.save
@@ -31,12 +37,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_description, :price, :prefecture_id, :category_id, :condition_id,
                                  :shipping_method_id, :shipping_time_id, :image).merge(user_id: current_user.id)
   end
+
   def item_find
     @item = Item.find(params[:id])
   end
