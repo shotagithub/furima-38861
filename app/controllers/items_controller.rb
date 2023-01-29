dd5202bb0692a110759f66e4ef045becc485744f
shotagithub <shota19891031@icloud.com>
2023-01-29 16:14:30 +0900

    item edit fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @item = Item.all.order(created_at: :desc)
   end
-
   def new
     @item = Item.new
   end
-
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -17,20 +16,13 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
-  def show
-    @item = Item.find(params[:id])
+  def show   
   end
-
   def edit
-    @item = Item.find(params[:id])
     return if user_signed_in? && current_user.id == @item.user.id
-
     redirect_to root_path
   end
-
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -44,5 +36,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_description, :price, :prefecture_id, :category_id, :condition_id,
                                  :shipping_method_id, :shipping_time_id, :image).merge(user_id: current_user.id)
+  end
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
