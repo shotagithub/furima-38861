e87a79cceb4d37c5ed8a819f8c97844923a4ce07
shotagithub <shota19891031@icloud.com>
2023-01-29 17:25:17 +0900

    destroy fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if user_signed_in? && current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+    
   end
 
   private
